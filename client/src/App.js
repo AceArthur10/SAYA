1e66235aba55d1cedb798fb48ccb16db3fca61d0
AceArthur10 <115504130+AceArthur10@users.noreply.github.com>
2023-04-11 15:38:19 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,56 @@
-import "./App.css"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Login from './Login'
-import ForgotPassword from './ForgotPassword';
+import "./Styles/App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider, AuthContext } from "./AuthContext";
+import Login from "./Login";
+import ForgotPassword from "./ForgotPassword";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
-import Settings from "./Settings"
+import Settings from "./Settings";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/ForgotPassword" element={<ForgotPassword/>} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path="/Settings" element={<Settings />} />
-      </Routes>
-    </Router>
-
+      <AuthProvider>
+        <Router>
+          <RoutesComponent />
+        </Router>
+      </AuthProvider>
     </div>
-
   );
 }
 
-export default App
+const RoutesComponent = () => {
+  const { isAuthenticated, userId } = useContext(AuthContext);
 
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/ForgotPassword" element={<ForgotPassword />} />
+      <Route path="/Register" element={<Register />} />
+      <Route
+        path="/Dashboard"
+        element={
+          isAuthenticated ? (
+            <Dashboard />
+          ) : (
+            <Navigate to="/" replace />
+          )
+        }
+      />
+      <Route
+        path="/Settings"
+        element={
+          isAuthenticated ? (
+            <Settings userId={userId} />
+          ) : (
+            <Navigate to="/" replace />
+          )
+        }
+      />
+    </Routes>
+  );
+};
 
-
+export default App;
